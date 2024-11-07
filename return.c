84cf05b37f2d288e338658327ae3d5550d887170
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-07 22:06:14 +0200

    making a programming with cli argument

---
+++ return.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing user Command-Line input\n");
+        return 1;
+    }
+    else
+    {
+        printf("Hello, %s!\n", argv[1]);
+        return 0;
+    }
+}
