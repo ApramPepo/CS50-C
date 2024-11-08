0a5ae2cbca0146261f98779b2d29c3275c6db5eb
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-08 23:57:09 +0200

    Practice Algorithms

--- search.c
+++ search.c
@@ -6,5 +6,15 @@ int main(void)
     int number[] = {30, 1000, 1, 10, 5, 50, 100, 200};
 
     int n = get_int("Number: ");
+    for (int i = 0; i < 7; i++)
+    {
+        if (number[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Program stopped, Not found\n");
+    return 1;
     
 }
