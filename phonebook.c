5e7601a3f5c19db7c6b06ae4dcae3736e56bc379
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-11 12:22:53 +0200

    Adding Algorithm

--- phonebook.c
+++ phonebook.c
@@ -2,17 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    string names;
+    string numbers;
+}
+person;
+
 int main(void)
 {
-    string names[] = {"Matthew", "Robin"};
-    string numbers[] = {"+1-777-777-7777", "+1-555-555-5555"};
+    person people[2];
+
+    people[0].names = "Matthew";
+    people[0].numbers = "+1-777-777-7777";
+
+    people[1].names = "Robin";
+    people[1].numbers = "+1-555-555-5555";
 
     string name = get_string("Name: ");
     for (int i = 0; i < 2; i++)
     {
-        if(strcmp(names[i], name) == 0)
+        if(strcmp(people[i].names, name) == 0)
         {
-            printf("Found %s\n %s\n", names, numbers);
+            printf("Found %s\n %s\n", people[i].names, people[i].numbers);
             return 0;
         }
     }
