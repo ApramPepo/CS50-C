479bd43da4dc963760b2455f9a572be3e4c6f592
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-21 20:59:15 +0200

    Using char instead of String

--- addresses.c
+++ addresses.c
@@ -4,6 +4,13 @@ int main(void)
 {
     char *s = "Hello";
     printf("%s\n", s);
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+    printf("%p\n", &s[4]);
+    printf("%p\n", &s[5]);
 }
 
 
