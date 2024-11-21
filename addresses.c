e5983db755252451a5207da94772aa0ad6db4a27
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-21 21:05:32 +0200

    Using star for addresses

--- addresses.c
+++ addresses.c
@@ -3,14 +3,12 @@
 int main(void)
 {
     char *s = "Hello";
-    printf("%s\n", s);
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
-    printf("%p\n", &s[4]);
-    printf("%p\n", &s[5]);
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
+    printf("%c\n", *(s+4));
+    printf("%c\n", *(s+5));
 }
 
 
