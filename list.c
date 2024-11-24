f3d5a5fb793a625ebfa72d838c9483acb8bc9caa
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-11-25 00:48:45 +0200

    program about database

--- list.c
+++ list.c
@@ -15,6 +15,24 @@ int main(void)
     list[2] = 3;
     list[3] = 4;
 
+    //
+
+    int *tmp = malloc(4 * sizeof(int))
+    if (tmp == NULL)
+    {
+        free(list);
+        printf("aborted\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4;
+
+    list = tmp;
+
     for (int i = 0; i < 4; i++)
     {
         printf("%i\n", list[i]);;
